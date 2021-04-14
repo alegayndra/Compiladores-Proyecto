ddfe6de03e6b6e3058a75bd536f4438c83a2415c
alegayndra <alegayndra@gmail.com>
2021-04-14 18:43:06 -0500

    Parser clase a medias

--- lib/parser/clase.rs
+++ lib/parser/clase.rs
@@ -1,86 +1,103 @@
-// use nom::{
-//   branch::alt,
-//   bytes::complete::tag,
-//   IResult,
-//   sequence::tuple,
-// };
+use nom::{
+  branch::alt,
+  bytes::complete::tag,
+  multi::many0,
+  IResult,
+  sequence::tuple,
+};
 
-// use crate::scanners::ws::*;
-// use crate::scanners::id::*;
-// use crate::parser::funcion::*;
-// use crate::parser::variables::*;
+use crate::scanners::ws::*;
+use crate::scanners::id::*;
+use crate::parser::funcion::*;
+use crate::parser::variables::*;
 
-// fn herencia(input: &str) -> IResult<&str, &str> {
-//   tuple((tag("<"), ws, id, ws, tag(">")))(input)
-//   .map(|(next_input, res)| {
-//     let (_, _, id, _, _,) = res;
-//     (next_input, id)
-//   })
-// }
+fn herencia(input: &str) -> IResult<&str, &str> {
+  tuple((tag("<"), ws, id, ws, tag(">")))(input)
+  .map(|(next_input, res)| {
+    let (_, _, id, _, _,) = res;
+    (next_input, id)
+  })
+}
 
-// fn posible_herencia(input: &str) -> IResult<&str, &str> {
-//   alt((herencia, ws))(input)
-// }
+fn posible_herencia(input: &str) -> IResult<&str, &str> {
+  alt((herencia, ws))(input)
+}
 
 // fn atributos(input: &str) -> IResult<&str, (&str, &str, Vec<(&str, (&str, Vec<&str>))>)> {
-//   variables(input)
-//   .map(|(next_input, res)| {
-//     (next_input, ("null", "variables", vec![res]))
-//   })
-// }
+fn atributos(input: &str) -> IResult<&str, (&str, &str, &str)> {
+  variables(input)
+  .map(|(next_input, _res)| {
+    // (next_input, ("null", "variables", vec![res]))
+    (next_input, ("null", "variables", "variables"))
+  })
+}
 
 // fn metodos(input: &str) -> IResult<&str, (&str, &str, Vec<(&str, (&str, Vec<&str>))>)> {
-//   funcion(input)
-//   .map(|(next_input, res)| {
-//     let (tipo, id, params) = res;
-//     let mut lista_params = Vec::new();
-//     for par in params {
-//       let (tipo_param, param) = par;
-//       lista_params.push((tipo_param, vec![param]))
-//     } 
-//     (next_input, (tipo, id, lista_params))
-//   })
-// }
+fn metodos(input: &str) -> IResult<&str, (&str, &str, &str)> {
+  funcion(input)
+  .map(|(next_input, res)| {
+    let (tipo, id, params) = res;
+    let mut lista_params = Vec::new();
+    for par in params {
+      let (tipo_param, param) = par;
+      lista_params.push((tipo_param, vec![param]))
+    } 
+    // (next_input, (tipo, id, lista_params))
+    (next_input, (tipo, id, "funcion"))
+  })
+}
 
 // fn variable_funcion(input: &str) -> IResult<&str, (&str, &str, Vec<(&str, (&str, Vec<&str>))>)> {
-//   alt((atributos, metodos))(input)
-// }
+fn variable_funcion(input: &str) -> IResult<&str, (&str, &str, &str)> {
+  alt((atributos, metodos))(input)
+}
 
 // pub fn clase(input: &str) -> IResult<&str, (&str, &str, (&str, &str, Vec<(&str, (&str, Vec<&str>))>))> {
-//   tuple((
-//     tag("clase"), necessary_ws, id, posible_herencia, tag("{"), variable_funcion, tag("}"), ws, tag(";") 
-//   ))
-//   (input)
-//   .map(|(next_input, res)| {
-//     let (_, _, id, padre, _, declaraciones, _, _, _) = res;
-//     (next_input, (id, padre, declaraciones))
-//   })
-// }
+pub fn clase(input: &str) -> IResult<&str, (&str, &str, Vec<(&str, &str, &str)>)> {
+  tuple((
+    tag("clase"), necessary_ws, id, posible_herencia, tag("{"), many0(variable_funcion), tag("}"), ws, tag(";") 
+  ))
+  (input)
+  .map(|(next_input, res)| {
+    let (_, _, id, padre, _, declaraciones, _, _, _) = res;
+    (next_input, (id, padre, declaraciones))
+  })
+}
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
-//   // use nom::{
-//   //     error::{ErrorKind, VerboseError, VerboseErrorKind},
-//   //     Err,
-//   // };
+#[cfg(test)]
+mod tests {
+  use super::*;
+  // use nom::{
+  //     error::{ErrorKind, VerboseError, VerboseErrorKind},
+  //     Err,
+  // };
 
-//   #[test]
-//   fn test_parametro() {
-//     assert_eq!(parametro("Persona id"), Ok(("", ("Persona", ("id", vec![])))));
-//     assert_eq!(parametro("entero id"), Ok(("", ("entero", ("id", vec![])))));
-//     assert_eq!(parametro("entero id[id]"), Ok(("", ("entero", ("id", vec!["id"])))));
-//   }
+  #[test]
+  fn test_herencia() {
+    assert_eq!(herencia("<Persona>"), Ok(("", "Persona")));
+    assert_eq!(herencia("< Persona >"), Ok(("", "Persona")));
+  }
 
-//   #[test]
-//   fn test_parametros_vacios() {
-//     assert_eq!(parametros_vacios("Persona id"), Ok(("Persona id", vec![("", ("", vec![]))])));
-//     assert_eq!(parametros_vacios("entero id"), Ok(("entero id", vec![("", ("", vec![]))])));
-//     assert_eq!(parametros_vacios("entero id[id]"), Ok(("entero id[id]", vec![("", ("", vec![]))])));
-//   }
+  #[test]
+  fn test_posible_herencia() {
+    assert_eq!(posible_herencia("<Persona>"), Ok(("", "Persona")));
+    assert_eq!(posible_herencia("< Persona >"), Ok(("", "Persona")));
+    assert_eq!(posible_herencia("< Persona"), Ok(("< Persona", "")));
+    assert_eq!(posible_herencia(":{}"), Ok((":{}", "")));
+  }
 
-//   #[test]
-//   fn test_funcion() {
-//     assert_eq!(funcion("void funcion func (entero var): { estatuto; regresa expresion ; }"), Ok(("", ("void", "func", vec![("entero", ("var", vec![]))]))));
-//   }
-// }
+  #[test]
+  fn test_atributos() {
+    // assert_eq!(atributos("Persona id, id;"), Ok(("",    ("Persona", vec![("id", vec![]), ("id", vec![])]))));
+    // assert_eq!(atributos("entero id[id][id];"), Ok(("", ("entero",  vec![("id", vec!["id","id"])]))));
+    assert_eq!(atributos("Persona id, id;"), Ok(("", ("null", "variables", "variables"))));
+    assert_eq!(atributos("entero id[id][id];"), Ok(("", ("null", "variables", "variables"))));
+  }
+
+  #[test]
+  fn test_metodos() {
+    // assert_eq!(atributos("Persona id, id;"), Ok(("",    ("Persona", vec![("id", vec![]), ("id", vec![])]))));
+    // assert_eq!(atributos("entero id[id][id];"), Ok(("", ("entero",  vec![("id", vec!["id","id"])]))));
+    assert_eq!(metodos("void funcion func (entero var): { estatuto; regresa expresion ; }"), Ok(("", ("void", "func", "funcion"))));
+  }
+}
