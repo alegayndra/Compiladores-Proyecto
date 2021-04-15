ba0764f71272c2b159c4c0e21e2a7535b8e440d8
alegayndra <alegayndra@gmail.com>
2021-04-14 19:45:03 -0500

    Borré comentarios innecesarios

--- lib/parser/llama_func.rs
+++ lib/parser/llama_func.rs
@@ -40,23 +40,11 @@ mod tests {
   //     Err,
   // };
 
-    #[test]
-    fn test_llama_func() {
-      assert_eq!(llama_func("id();"), Ok(("", ("id", vec![], ("expresiones", vec![])))));
-      assert_eq!(llama_func("id.metodo();"), Ok(("", ("id", vec!["metodo"], ("expresiones", vec![])))));
-      assert_eq!(llama_func("id(expresion);"), Ok(("", ("id", vec![], ("expresiones", vec!["expresion"])))));
-      assert_eq!(llama_func("id.metodo.metodo2(expresion);"), Ok(("", ("id", vec!["metodo", "metodo2"], ("expresiones", vec!["expresion"])))));
-    }
-
-    // #[test]
-    // fn test_parametros_vacios() {
-    //   assert_eq!(parametros_vacios("Persona id"), Ok(("Persona id", vec![("", ("", vec![]))])));
-    //   assert_eq!(parametros_vacios("entero id"), Ok(("entero id", vec![("", ("", vec![]))])));
-    //   assert_eq!(parametros_vacios("entero id[id]"), Ok(("entero id[id]", vec![("", ("", vec![]))])));
-    // }
-
-    // #[test]
-    // fn test_funcion() {
-    //   assert_eq!(funcion("void funcion func (entero var): { estatuto; regresa expresion ; }"), Ok(("", ("void", "func", vec![("entero", ("var", vec![]))]))));
-    // }
+  #[test]
+  fn test_llama_func() {
+    assert_eq!(llama_func("id();"), Ok(("", ("id", vec![], ("expresiones", vec![])))));
+    assert_eq!(llama_func("id.metodo();"), Ok(("", ("id", vec!["metodo"], ("expresiones", vec![])))));
+    assert_eq!(llama_func("id(expresion);"), Ok(("", ("id", vec![], ("expresiones", vec!["expresion"])))));
+    assert_eq!(llama_func("id.metodo.metodo2(expresion);"), Ok(("", ("id", vec!["metodo", "metodo2"], ("expresiones", vec!["expresion"])))));
+  }
 }
