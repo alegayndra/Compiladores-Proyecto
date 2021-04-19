2d417f168f46cf643dbda733ad0acd7de94ccc68
alegayndra <alegayndra@gmail.com>
2021-04-19 12:54:46 -0500

    Completé parser programa

--- lib/parser/programa.rs
+++ lib/parser/programa.rs
@@ -9,10 +9,10 @@ use crate::scanners::id::*;
 use crate::parser::declaraciones::declaraciones::*;
 
 pub fn programa(input: &str) -> IResult<&str, (&str, Vec<&str>, &str)> {
-  tuple((tag("programa"), necessary_ws, id, ws, tag(";"), ws, declaraciones, ws, tag("principal()"), ws, tag("bloque")))
+  tuple((ws, tag("programa"), necessary_ws, id, ws, tag(";"), ws, declaraciones, ws, tag("principal()"), ws, tag("bloque"), ws))
   (input)
   .map(|(next_input, res)| {
-    let (_, _, id, _, _, _, declaraciones, _, _, _, bloque) = res;
+    let (_, _, _, id, _, _, _, declaraciones, _, _, _, bloque, _) = res;
     (next_input, (id, declaraciones, bloque))
   })
 }
@@ -30,5 +30,41 @@ mod tests {
     // assert_eq!(programa("programa idPrograma; clase principal() bloque"), Ok(("", ("idPrograma", vec!["clase"], "bloque"))));
     // assert_eq!(programa("programa idPrograma; principal() bloque"), Ok(("", ("idPrograma", vec![], "bloque"))));
     // assert_eq!(programa("programa idPrograma; clase, variables principal() bloque"), Ok(("", ("idPrograma", vec!["clase", "variables"], "bloque"))));
+    assert_eq!(programa("
+      programa idPrograma;
+      principal() bloque"
+    ), Ok(("", ("idPrograma", vec![], "bloque"))));
+    assert_eq!(programa("
+      programa idPrograma;
+      entero num;
+      principal() bloque"
+    ), Ok(("", ("idPrograma", vec!["variables"], "bloque"))));
+    assert_eq!(programa("
+      programa idPrograma;
+      clase Estudiante <Persona> {
+        char nombre[10], apellido[10];
+      };
+      principal() bloque"
+    ), Ok(("", ("idPrograma", vec!["clase"], "bloque"))));
+    assert_eq!(programa("
+      programa idPrograma;
+      void funcion func (entero var): {
+        estatuto;
+        regresa expresion;
+      }
+      principal() bloque"
+    ), Ok(("", ("idPrograma", vec!["funcion"], "bloque"))));
+    assert_eq!(programa("
+      programa idPrograma;
+      void funcion func (entero var): {
+        estatuto;
+        regresa expresion;
+      }
+      entero num;
+      clase Estudiante <Persona> {
+        char nombre[10], apellido[10];
+      };
+      principal() bloque"
+    ), Ok(("", ("idPrograma", vec!["funcion", "variables", "clase"], "bloque"))));
   }
 }
