2d6e5fa6d777a442ad132c4676de533743af01ba
Jcarlos0828 <jose.carlos4013@gmail.com>
2021-05-15 18:28:03 -0500

    Created dimension_decl parser

---
+++ lib/parser/dimensiones_decl.rs
@@ -0,0 +1,85 @@
+use nom::{
+  branch::alt,
+  bytes::complete::tag,
+  IResult,
+  sequence::tuple,
+};
+
+use crate::scanners::ws::*;
+use crate::scanners::constantes::*;
+
+pub fn dimension_decl(input: &str) -> IResult<&str, Vec<&str>> {
+  tuple((tag("["), ws, num_entero, ws, tag("]")))(input)
+  .map(|(next_input, res)| {
+    let (_, _, dimension, _, _) = res;
+    (next_input, vec![dimension])
+  })
+}
+
+fn dos_dimensiones_decl(input: &str) -> IResult<&str, Vec<&str>> {
+  tuple((dimension, ws, dimension))
+  (input)
+  .map(|(next_input, res)| {
+    let (dimension_1, _, dimension_2) = res;
+    (next_input, vec![dimension_1[0], dimension_2[0]])
+  })
+}
+
+priv pub fn ws_vec_decl(input: &str) -> IResult<&str, Vec<&str>> {
+  Ok((input, vec![]))
+}
+
+pub fn con_dim_decl(input: &str) -> IResult<&str, Vec<&str>> {
+  alt((dos_dimensiones, dimension, ws_vec))
+  (input)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  // use nom::{
+  //     error::{ErrorKind, VerboseError, VerboseErrorKind},
+  //     Err,
+  // };
+
+  #[test]
+  fn test_dimension_decl() {
+    // assert_eq!(dimension("[id]"), Ok(("", vec!["id"])));
+    // assert_eq!(dimension("[ id ]"), Ok(("", vec!["id"])));
+    // assert_eq!(dimension("[  id  ]"), Ok(("", vec!["id"])));
+
+    assert_eq!(dimension_decl("[1 ]"),     Ok(("", vec!["expresion"])));
+    assert_eq!(dimension_decl("[ 78]"),   Ok(("", vec!["expresion"])));
+    assert_eq!(dimension_decl("[  69  ]"), Ok(("", vec!["expresion"])));
+  }
+
+  #[test]
+  fn test_dos_dimensiones_decl() {
+    // assert_eq!(dos_dimensiones("[id][id]"), Ok(("", vec!["id", "id"])));
+    // assert_eq!(dos_dimensiones("[ id ][ id ]"), Ok(("", vec!["id", "id"])));
+    // assert_eq!(dos_dimensiones("[  id  ][  id  ]"), Ok(("", vec!["id", "id"])));
+
+    assert_eq!(dos_dimensiones_decl("[420][2]"),         Ok(("", vec!["expresion", "expresion"])));
+    assert_eq!(dos_dimensiones_decl("[ 69666][ 0 ]"),     Ok(("", vec!["expresion", "expresion"])));
+    assert_eq!(dos_dimensiones_decl("[  1  ][   2 ]"), Ok(("", vec!["expresion", "expresion"])));
+  }
+
+  #[test]
+  fn test_ws_vec_decl() {
+    assert_eq!(ws_vec_decl("aaaa"), Ok(("aaaa", vec![])));
+    assert_eq!(ws_vec_decl("bbbb"), Ok(("bbbb", vec![])));
+    assert_eq!(ws_vec_decl("cccc"), Ok(("cccc", vec![])));
+    assert_eq!(ws_vec_decl("    "), Ok(("    ", vec![])));
+  }
+
+  #[test]
+  fn test_con_dim_decl() {
+    // assert_eq!(con_dim("[id]"), Ok(("", vec!["id"])));
+    // assert_eq!(con_dim("[id][id]"), Ok(("", vec!["id", "id"])));
+    // assert_eq!(con_dim("aaaa"), Ok(("aaaa", vec![])));
+
+    assert_eq!(con_dim_decl("[id]"), Ok(("", vec!["expresion"])));
+    assert_eq!(con_dim_decl("[id][id]"), Ok(("", vec!["expresion", "expresion"])));
+    assert_eq!(con_dim_decl("aaaa"), Ok(("aaaa", vec![])));
+  }
+}
