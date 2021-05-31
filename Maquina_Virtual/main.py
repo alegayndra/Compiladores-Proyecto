3dccae0a26caec5fa0c759c0754187332c763ab7
Jcarlos0828 <jose.carlos4013@gmail.com>
2021-05-30 19:49:01 -0500

    VM now reads functions data

--- Maquina_Virtual/main.py
+++ Maquina_Virtual/main.py
@@ -48,6 +48,8 @@ dir_memoria = [
   ]
 ]
 
+funciones = []
+
 auxLocales = [ # Locales
     [ # Enteras
       # Inicio  Limite
@@ -78,7 +80,7 @@ mapa_memoria = [
       [],  [] # Normales
     ]
   ],
-  [ #000 Locales
+  [ # Locales
    # Aqui se van a pushear stacks de memoria
   ],
   [ # Constantes
@@ -305,7 +307,7 @@ def switchCubo(cuadruplo):
     # print(type(cuadruplo[3]))
     # print(cuadruplo[3])
     # print(mapa_memoria[0][0])
-    print(extaerMemoria[cuadruplo[1]])
+    print(extaerMemoria(cuadruplo[1]))
     if not extaerMemoria(cuadruplo[1]):
       print("entramos")
       num_cuadruplo[0] = cuadruplo[3] - 1
@@ -322,7 +324,6 @@ def switchCubo(cuadruplo):
     return
 
 def ejecutar_programa():
-  print(len(lista_cuadruplos))
   while num_cuadruplo[0] < len(lista_cuadruplos):
     print("Ejecutamos el cuadruplo #", num_cuadruplo[0])
     print(lista_cuadruplos[num_cuadruplo[0]])
@@ -351,6 +352,60 @@ def leerCuadruplos(txt_cuadruplos):
     readStr = txt_cuadruplos.find('\n', readStr) + 1
   ejecutar_programa()
 
+def guardarParams(direcciones_params, i):
+  readStr = 0
+  count = 0
+  parm = []
+  while readStr < len(direcciones_params) and count < 6:
+    cantidades = direcciones_params[readStr:direcciones_params.find('\n', readStr)]
+    direccion = int(cantidades[1:cantidades.find(',')] )
+    cantidades = cantidades[cantidades.find(',') + 2:]
+    tipo = cantidades[:cantidades.find(')')]
+    readStr = direcciones_params.find('\n', readStr) + 1
+    parm.append((direccion, tipo))
+  funciones[i].append(parm)
+
+def guardarFunciones(direcciones_funcs):
+  readStr = 0
+  i = 0
+  while readStr < len(direcciones_funcs):
+    funciones.append([])
+    cantidades = direcciones_funcs[readStr:direcciones_funcs.find('\n', readStr)]
+    nombreFunc = direcciones_funcs[1:cantidades.find(',')]
+    cantidades = cantidades[cantidades.find(',') + 2:]
+
+    direcFunc = int(cantidades[:cantidades.find(',')])
+    cantidades = cantidades[cantidades.find(',') + 2:]
+    numCuadruplo = int(cantidades[:cantidades.find(')')])
+    readStr = direcciones_funcs.find('\n', readStr) + 1
+
+    cantidades = direcciones_funcs[readStr:direcciones_funcs.find('\n', readStr)]
+    cntIntNormal = int(cantidades[1:cantidades.find(',')])
+    cantidades = cantidades[cantidades.find(',') + 2:]
+    cntIntTemp = int(cantidades[:cantidades.find(')')])
+    readStr = direcciones_funcs.find('\n', readStr) + 1
+
+    cantidades = direcciones_funcs[readStr:direcciones_funcs.find('\n', readStr)]
+    readStr = direcciones_funcs.find('\n', readStr) + 1
+    cntFloatNormal = int(cantidades[1:cantidades.find(',')])
+    cantidades = cantidades[cantidades.find(',') + 2:]
+    cntFloatTemp = int(cantidades[:cantidades.find(')')])
+
+    cantidades = direcciones_funcs[readStr:direcciones_funcs.find('\n', readStr)]
+    readStr = direcciones_funcs.find('\n', readStr) + 1
+    cntCharNormal = int(cantidades[1:cantidades.find(',')])
+    cantidades = cantidades[cantidades.find(',') + 2:]
+    cntCharTemp = int(cantidades[:cantidades.find(')')])
+    funciones[i].append((nombreFunc, direcFunc, numCuadruplo))
+    funciones[i].append((cntIntNormal, cntIntTemp))
+    funciones[i].append((cntFloatNormal, cntFloatTemp))
+    funciones[i].append((cntCharNormal, cntCharTemp))
+    guardarParams(direcciones_funcs[direcciones_funcs.find("PARAMS") + 7:direcciones_funcs.find("FIN_PARAMS")], i)
+    direcciones_funcs = direcciones_funcs[direcciones_funcs.find("FIN_PARAMS", readStr) + 11:direcciones_funcs.find("FIN_FUNCIONES", readStr)]
+    readStr = 0
+    print(direcciones_funcs)
+    i += 1
+  
 def guardarMapaGlobs(direcciones_globs):
   readStr = 0
   cantidades = direcciones_globs[readStr:direcciones_globs.find('\n', readStr)]
@@ -391,7 +446,6 @@ def guardarMapaCons(direcciones_const):
   mapa_memoria[2][2][0] = [None] * int(cntChar)
 
   direcciones_const = direcciones_const[direcciones_const.find('\n') + 1:]
-  
   while readStr < len(direcciones_const):
     valYdir = direcciones_const[readStr:direcciones_const.find('\n', readStr)]
 
@@ -428,7 +482,10 @@ def leer_obj():
   #Registro de valores para globales en mapa de memoria
   guardarMapaGlobs(stringTxt[stringTxt.find("GLOBALES") + 9:stringTxt.find("FIN_GLOBALES")])
 
+  guardarFunciones(stringTxt[stringTxt.find("FUNCIONES") + 10:stringTxt.find("FIN_FUNCIONES")])
+  
   leerCuadruplos(stringTxt[stringTxt.find("CUADRUPLOS") + 11:stringTxt.find("FIN_CUADRUPLOS")])
+  '''
   print("Aqui estan las variables globales")
   print(mapa_memoria[0])
   print("")
@@ -453,6 +510,6 @@ def leer_obj():
   print("")
   print("Aqui estan las variables CHAR globales")
   print(mapa_memoria[2][2])
-
+  '''
 leer_obj()
 ejecutar_programa()
